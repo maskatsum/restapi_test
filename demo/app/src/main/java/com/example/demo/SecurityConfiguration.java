631572b49c953e01a37b98e4d26a2fe419e21008
masashi katsumata <wf9a5m75@gmail.com>
2024-03-11 21:16:37 -0700

    test

--- demo/app/src/main/java/com/example/demo/SecurityConfiguration.java
+++ demo/app/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,40 +1,129 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequestEntityConverter;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration  {
-    
+
+	@Value("${spring.security.oauth2.client.provider.external.issuer-uri}")
+	private String issuer;
+	@Value("${spring.security.oauth2.client.registration.example.client-id}")
+	private String clientId;
+
+	LogoutHandler oidcLogoutHandler() {
+		return (request, response, authentication) -> {
+			try {
+				response.sendRedirect(issuer + "v2/logout?client_id=" + clientId + "&returnTo=http://localhost:8080/");
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		};
+	}
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		
-		http.oauth2Client().and()
-			.oauth2Login()
-			.tokenEndpoint().and()
-			.userInfoEndpoint();
+//		http.oauth2Client().and()
+//			.oauth2Login()
+//			.tokenEndpoint().and()
+//			.userInfoEndpoint();
+		http.oauth2Login(Customizer.withDefaults());
 		
-		http.csrf(csrf -> csrf.disable())
-			.authorizeHttpRequests(auth -> auth
-					.requestMatchers("/unauthenticated", "/oauth2/**", "/login/**", "/welcome").permitAll()
-					.anyRequest().fullyAuthenticated()
-//			)
-//			.logout(logout -> logout
-///					.logoutSuccessUrl("http://localhost:8080/welcome")
-			);
-		
+		http.csrf(AbstractHttpConfigurer::disable);
+		http.cors(AbstractHttpConfigurer::disable);
+
+		http.authorizeHttpRequests(auth -> auth
+			.requestMatchers("/unauthenticated", "/oauth2/**", "/login/**", "/welcome").permitAll()
+			.anyRequest().fullyAuthenticated()
+		);
+
+//		http.logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//				.addLogoutHandler(oidcLogoutHandler()));
+
 		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
 		
 		return http.build();
 	}
+
+	@Bean
+	public OAuth2AuthorizedClientManager authorizedClientManager(
+			ClientRegistrationRepository clientRegistrationRepository,
+			OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+		OAuth2AuthorizedClientProvider authorizedClientProvider =
+				OAuth2AuthorizedClientProviderBuilder.builder()
+						.authorizationCode()
+						.refreshToken()
+						.clientCredentials()
+//						.password()
+						.build();
+
+		DefaultOAuth2AuthorizedClientManager authorizedClientManager =
+				new DefaultOAuth2AuthorizedClientManager(
+						clientRegistrationRepository, authorizedClientRepository);
+		authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+		return authorizedClientManager;
+	}
+	private ClientRegistration keyCloakClientRegistration() {
+		return ClientRegistration.withRegistrationId("google")
+				.clientId("google-client-id")
+				.clientSecret("google-client-secret")
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+				.scope("openid", "profile", "email", "address", "phone")
+				.authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+				.tokenUri("https://www.googleapis.com/oauth2/v4/token")
+				.userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+				.userNameAttributeName(IdTokenClaimNames.SUB)
+				.jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
+				.clientName("Google")
+				.build();
+	}
+
+	private static Converter<OAuth2AuthorizationCodeGrantRequest, MultiValueMap<String, String>> parametersConverter() {
+		return (grantRequest) -> {
+			MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+			parameters.set("audience", "xyz_value");
+
+			return parameters;
+		};
+	}
+
 }
